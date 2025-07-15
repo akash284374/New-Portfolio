7759b61da2c6ae6cbf6455cfef035b73676e55eb
Akash <akash284374@gmail.com>
2025-07-16 01:09:50 +0530

    cv  updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -29,7 +29,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1pgQDZbxjzBJYSutptVGFPLyrUfXBOgcx/view?usp=sharing" target="_blank"
+                href="https://drive.google.com/file/d/1JxJFtyDqTabqcNY33SjeyGRYj0WVHoc0/view?usp=sharing" target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
